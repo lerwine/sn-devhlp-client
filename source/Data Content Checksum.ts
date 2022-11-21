6b5da7e639042a995895e3a8deec1231290965b8
Leonard <lenny@erwinefamily.net>
2022-11-21 15:12:09 -0500

    fixed bug

--- source/Data Content Checksum.ts
+++ source/Data Content Checksum.ts
@@ -63,15 +63,15 @@ var api: { controller: function } = function() {
     }
 
     function calculateNextCrc64(buffer: Int8Array & { length: 8 }, value: number): void {
-        var v = crc64Table[(value ^ crc.buffer[0]) & 0xff];
-        crc.buffer[0] = crc.buffer[1];
-        crc.buffer[1] = crc.buffer[2];
-        crc.buffer[2] = crc.buffer[3];
-        crc.buffer[3] = crc.buffer[4];
-        crc.buffer[4] = crc.buffer[5];
-        crc.buffer[5] = crc.buffer[6];
-        crc.buffer[6] = crc.buffer[7] ^ (v & 0xff);
-        crc.buffer[7] = v >> 8;
+        var v = crc64Table[(value ^ buffer[0]) & 0xff];
+        buffer[0] = buffer[1];
+        buffer[1] = buffer[2];
+        buffer[2] = buffer[3];
+        buffer[3] = buffer[4];
+        buffer[4] = buffer[5];
+        buffer[5] = buffer[6];
+        buffer[6] = buffer[7] ^ (v & 0xff);
+        buffer[7] = v >> 8;
     }
 
     function calculateCrc64(text?: string | null) : ICrc64 {
